c87acd11096157ffd8969943c536c54d4a66f9d4
AnicetCelerier <anicet.celerier@gmail.com>
2022-06-27 16:26:30 +0200

    paramétrage sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Page, ProjectsComponent } from "../components";
 import { fetchProjects } from "../components/Page";
 import { Project } from "../types";
+import "../index.scss";
+
 
 type HomeProps = {
   projects: Project[];
