a591bc97e0eee30d47df7c8b786ba0a2ec2d0a46
AnicetCelerier <anicet.celerier@gmail.com>
2022-06-28 15:43:44 +0200

    issue4 premier jet

--- components/Home/ProjectsComponent/index.tsx
+++ components/Home/ProjectsComponent/index.tsx
@@ -5,18 +5,36 @@ type ProjectsComponentProps = {
 };
 
 const ProjectsComponent = ({ projects }: ProjectsComponentProps) => {
+  projects.sort((a: any, b: any) => parseFloat(a.order) - parseFloat(b.order));
+
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project.title}>
-          <h1>{project.title}</h1>
-          <p>{project.content}</p>
-          <a href={project.link}>
-            <img src={project.image} />
-          </a>
+    <section className="joazco--block-3" id="projects">
+      <article>
+        <h2>Nos projets</h2>
+        <div className="joazco--block-3-content">
+          {projects.map((project) => (
+            <div className="joazco--block-3-content-project">
+              <div key={project.title}>
+                <h3>{project.title}</h3>
+                <p>{project.content}</p>
+                <div className="joazco--block-3-content-link-content">
+                <a href={project.link}>
+                  {/* attribut alt ? */}
+                  <img src={project.image} />
+                  <div>
+                      <span>
+                        Lien vers {project.link.replace(/(^\w+:|^)\/\//, '')}
+                        <i className="fas fa-external-link-alt"></i>
+                      </span>
+                    </div>
+                </a>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </article>
+    </section>
   );
 };
 
