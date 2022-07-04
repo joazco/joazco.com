fc10853506284506fd01566062e8de46744442cf
AnicetCelerier <anicet.celerier@gmail.com>
2022-07-04 14:37:30 +0200

    formulaire

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, TextField, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { Page } from "../components";
 
@@ -8,9 +8,33 @@ const Admin = () => {
   // }
   return (
     <Page>
-      <Button variant="contained" endIcon={<SendIcon />}>
-        Test
-      </Button>
+      <div className="admin">
+        <img src="https://firebasestorage.googleapis.com/v0/b/joazco-1fd73.appspot.com/o/images%2Fjoazco_logo_blank_72x72.png?alt=media&token=00e5c472-469e-4683-8653-9205d0417fe5" />
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Login"
+            variant="outlined"
+            required
+          />
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            required
+          />
+          <Button variant="contained" /* endIcon={<SendIcon />} */>
+            ok
+          </Button>
+        </Box>
+      </div>
     </Page>
   );
 };
