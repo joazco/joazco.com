7803ed42d0b4afa9705ec87acbd3493074ff5ae2
Jordan <jazoulay@joazco.com>
2022-07-05 11:30:02 +0200

    debug

--- hooks/useAdmin/index.tsx
+++ hooks/useAdmin/index.tsx
@@ -1,21 +1,16 @@
 import { useState, FormEvent } from "react";
-<<<<<<< HEAD
 import {
   getAuth,
   signInWithEmailAndPassword,
   User,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
-=======
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
->>>>>>> cc4789ad94dbbf859697c2fbedf313a79d189964
 
 const useAdmin = () => {
   const [user, setUser] = useState<User | null | undefined>(undefined);
   const [emailInput, setEmailInput] = useState<string>("");
   const [passwordInput, setPasswordInput] = useState<string>("");
 
-<<<<<<< HEAD
   // const login = e => {
   //     e.preventDefault();
   //     const auth = getAuth();
@@ -41,30 +36,6 @@ const useAdmin = () => {
       }
     });
   };
-=======
-// const login = e => {
-//     e.preventDefault();
-//     const auth = getAuth();
-//     console.log("🚀 ~ file: index.tsx ~ line 13 ~ handleSubmit ~ auth ", auth )
-    
-    // signInWithEmailAndPassword(auth, emailInput, passwordInput)
-    //   .then((userCredential) => {
-    //     const user = userCredential.user;
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //   });
-    // }
-    const connectionWithEmailPassword = (emailInput: string, passwordInput: string) => {
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, emailInput, passwordInput).catch((err) => {
-          if (err.message.includes("auth/user-not-found")) {
-            createUserWithEmailAndPassword(auth, emailInput, passwordInput);
-          }
-        });
-      };
->>>>>>> cc4789ad94dbbf859697c2fbedf313a79d189964
 
   return {
     // user,
@@ -73,11 +44,7 @@ const useAdmin = () => {
     passwordInput,
     setPasswordInput,
     // signInWithEmailAndPassword,
-<<<<<<< HEAD
     connectionWithEmailPassword,
-=======
-    connectionWithEmailPassword
->>>>>>> cc4789ad94dbbf859697c2fbedf313a79d189964
   };
 };
 
