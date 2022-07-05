b49ac910299a6ba797de45ee646c9ffc5485194d
Jordan <jazoulay@joazco.com>
2022-07-05 11:27:38 +0200

    add NEXT_PUBLIC all env vars

--- hooks/useAdmin/index.tsx
+++ hooks/useAdmin/index.tsx
@@ -1,33 +1,41 @@
 import { useState, FormEvent } from "react";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  User,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 const useAdmin = () => {
   const [user, setUser] = useState<User | null | undefined>(undefined);
   const [emailInput, setEmailInput] = useState<string>("");
   const [passwordInput, setPasswordInput] = useState<string>("");
 
-// const login = e => {
-//     e.preventDefault();
-//     const auth = getAuth();
-//     console.log("🚀 ~ file: index.tsx ~ line 13 ~ handleSubmit ~ auth ", auth )
-    
-    // signInWithEmailAndPassword(auth, emailInput, passwordInput)
-    //   .then((userCredential) => {
-    //     const user = userCredential.user;
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //   });
-    // }
-    const connectionWithEmailPassword = (emailInput: string, passwordInput: string) => {
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, emailInput, passwordInput).catch((err) => {
-          if (err.message.includes("auth/user-not-found")) {
-            createUserWithEmailAndPassword(auth, emailInput, passwordInput);
-          }
-        });
-      };
+  // const login = e => {
+  //     e.preventDefault();
+  //     const auth = getAuth();
+  //     console.log("🚀 ~ file: index.tsx ~ line 13 ~ handleSubmit ~ auth ", auth )
+
+  // signInWithEmailAndPassword(auth, emailInput, passwordInput)
+  //   .then((userCredential) => {
+  //     const user = userCredential.user;
+  //   })
+  //   .catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //   });
+  // }
+  const connectionWithEmailPassword = (
+    emailInput: string,
+    passwordInput: string
+  ) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, emailInput, passwordInput).catch((err) => {
+      if (err.message.includes("auth/user-not-found")) {
+        createUserWithEmailAndPassword(auth, emailInput, passwordInput);
+      }
+    });
+  };
 
   return {
     // user,
@@ -36,7 +44,7 @@ const useAdmin = () => {
     passwordInput,
     setPasswordInput,
     // signInWithEmailAndPassword,
-    connectionWithEmailPassword
+    connectionWithEmailPassword,
   };
 };
 
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,13 +2,13 @@ import { Button, TextField, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { Page } from "../components";
 import { useAdmin } from "../hooks";
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useCallback } from "react";
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
-import { app } from "../components/Page";
+import { auth } from "../components/Page";
 
 const Admin = () => {
   // if (!connected) {
@@ -27,60 +27,56 @@ const Admin = () => {
   const [emailInput, setEmailInput] = useState<string>("");
   const [passwordInput, setPasswordInput] = useState<string>("");
 
-  const connectionWithEmailPassword = (
-    emailInput: string,
-    passwordInput: string
-  ) => {
-    console.log(
-      "🚀 ~ file: admin.tsx ~ line 27 ~ connectionWithEmailPassword ~ passwordInput",
-      passwordInput
-    );
-    console.log(
-      "🚀 ~ file: admin.tsx ~ line 27 ~ connectionWithEmailPassword ~ emailInput",
-      app
-    );
-    const auth = getAuth(app);
-    signInWithEmailAndPassword(auth, emailInput, passwordInput);
-  };
+  const connectionWithEmailPassword = useCallback(
+    (emailInput: string, passwordInput: string) => {
+      // const auth = getAuth(app);
+      signInWithEmailAndPassword(auth, emailInput, passwordInput);
+    },
+    []
+  );
 
   return (
     <Page>
-      <div className="admin">
-        <img src="https://firebasestorage.googleapis.com/v0/b/joazco-1fd73.appspot.com/o/images%2Fjoazco_logo_blank_72x72.png?alt=media&token=00e5c472-469e-4683-8653-9205d0417fe5" />
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            required
-            value={emailInput}
-            onChange={(e) => setEmailInput(e.target.value)}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            required
-            value={passwordInput}
-            onChange={(e) => setPasswordInput(e.target.value)}
-          />
-          <Button
-            variant="contained"
-            /* endIcon={<SendIcon />} */ onClick={() =>
-              connectionWithEmailPassword(emailInput, passwordInput)
-            }
+      <form>
+        <div className="admin">
+          <img src="https://firebasestorage.googleapis.com/v0/b/joazco-1fd73.appspot.com/o/images%2Fjoazco_logo_blank_72x72.png?alt=media&token=00e5c472-469e-4683-8653-9205d0417fe5" />
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
           >
-            Log In
-          </Button>
-        </Box>
-      </div>
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              type="email"
+              required
+              value={emailInput}
+              onChange={(e) => setEmailInput(e.target.value)}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              type="password"
+              required
+              value={passwordInput}
+              onChange={(e) => setPasswordInput(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              /* endIcon={<SendIcon />} */ onClick={() =>
+                connectionWithEmailPassword(emailInput, passwordInput)
+              }
+            >
+              Log In
+            </Button>
+          </Box>
+        </div>
+      </form>
     </Page>
   );
 };
